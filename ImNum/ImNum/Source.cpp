92b547cc4e58005e9bd57c3b28a27baf14bf5b98
Ната <nattaa2904@gmail.com>
2023-03-09 20:37:11 +0300

    лямбды
    потом надо добавить пересылку компаратора по указателю, а не шаблону

--- ImNum/ImNum/Source.cpp
+++ ImNum/ImNum/Source.cpp
@@ -2,57 +2,75 @@
 #include <iostream>
 #include <vector>
 
-class SortInc
-{
-public:
-	template<class T, class T1>
-	void operator()(vector<ImNum<T>>& mass, T1 comp)
-	{
-		ImNum<T> C;
-		size_t len = mass.size();
-		cout << "в массиве " << len << " элементов" << endl;
-		for (int i = 0; i < len; i++) {
-			for (int j = 0; j < len - 1; j++) {
-				if (comp(mass[j], mass[j + 1]) == 1) {
-					C = mass[j];
-					mass[j] = mass[j + 1];
-					mass[j + 1] = C;
-				}
-			}
-		}
-	}
-} SortInc;
+//template<class T, class T1>
+//void SortIncAbs(vector<ImNum<T>>& mass)
+//{
+//
+//	ImNum<T> C;
+//	size_t len = mass.size();
+//	cout << "в массиве " << len << " элементов" << endl;
+//	for (int i = 0; i < len; i++) {
+//		for (int j = 0; j < len - 1; j++) {
+//			if (comp(mass[j], mass[j + 1]) == 1) {
+//				C = mass[j];
+//				mass[j] = mass[j + 1];
+//				mass[j + 1] = C;
+//			}
+//		}
+//	}
+//}
 
-class CompAbs
-{
-public:
-	template<class T>
-	int operator()(const ImNum<T>& v1, const ImNum<T>& v2)
-	{
-		typename AbsType<T>::type a1 = abs(v1), a2 = abs(v2);
-		return a1 > a2 ? 1 : (a1 < a2 ? -1 : 0);
-	}
-}CompAbs;
-
-class CompReal
-{
-public:
-	template<class T>
-	int operator()(const ImNum<T>& v1, const ImNum<T>& v2)
-	{
-		return v1.giveR() > v2.giveR() ? 1 : (v1.giveR() < v2.giveR() ? -1 : 0);
-	}
-}CompReal;
-
-class CompIm
-{
-public:
-	template<class T>
-	int operator()(const ImNum<T>& v1, const ImNum<T>& v2)
-	{
-		return v1.giveI() > v2.giveI() ? 1 : (v1.giveI() < v2.giveI() ? -1 : 0);
-	}
-}CompIm;
+//class SortInc
+//{
+//public:
+//	template<class T, class T1>
+//	void operator()(vector<ImNum<T>>& mass, T1 comp)
+//	{
+//		ImNum<T> C;
+//		size_t len = mass.size();
+//		cout << "в массиве " << len << " элементов" << endl;
+//		for (int i = 0; i < len; i++) {
+//			for (int j = 0; j < len - 1; j++) {
+//				if (comp(mass[j], mass[j + 1]) == 1) {
+//					C = mass[j];
+//					mass[j] = mass[j + 1];
+//					mass[j + 1] = C;
+//				}
+//			}
+//		}
+//	}
+//} SortInc;
+// 
+//class CompAbs
+//{
+//public:
+//	template<class T>
+//	int operator()(const ImNum<T>& v1, const ImNum<T>& v2)
+//	{
+//		typename AbsType<T>::type a1 = abs(v1), a2 = abs(v2);
+//		return a1 > a2 ? 1 : (a1 < a2 ? -1 : 0);
+//	}
+//}CompAbs;
+// 
+//class CompReal
+//{
+//public:
+//	template<class T>
+//	int operator()(const ImNum<T>& v1, const ImNum<T>& v2)
+//	{
+//		return v1.giveR() > v2.giveR() ? 1 : (v1.giveR() < v2.giveR() ? -1 : 0);
+//	}
+//}CompReal;
+// 
+//class CompIm
+//{
+//public:
+//	template<class T>
+//	int operator()(const ImNum<T>& v1, const ImNum<T>& v2)
+//	{
+//		return v1.giveI() > v2.giveI() ? 1 : (v1.giveI() < v2.giveI() ? -1 : 0);
+//	}
+//}CompIm;
 
 //template<class T, class T1>
 //void SortInc(vector<ImNum<T>> & mass, T1 comp)
@@ -70,7 +88,7 @@ public:
 //		}
 //	}
 //}
-
+//
 //template<class T>
 //int CompAbs(const ImNum<T>& v1, const ImNum<T>& v2) //сравнение 2 элем по модулю
 //{
@@ -78,10 +96,30 @@ public:
 //	return a1 > a2 ? 1 : (a1 < a2 ? -1 : 0);
 //}
 
+template<class T>
+auto CompAbs = [](const ImNum<T>& v1, const ImNum<T>& v2) //сравнение 2 элем по модулю
+{
+	typename AbsType<T>::type a1 = abs(v1), a2 = abs(v2);
+	return a1 > a2 ? 1 : (a1 < a2 ? -1 : 0);
+};
+
+template<class T>
+auto CompReal = [](const ImNum<T>& v1, const ImNum<T>& v2) //сравнение 2 элем по модулю
+{
+	return v1.giveR() > v2.giveR() ? 1 : (v1.giveR() < v2.giveR() ? -1 : 0);
+};
+
+template<class T>
+auto CompIm = [](const ImNum<T>& v1, const ImNum<T>& v2) //сравнение 2 элем по модулю
+{
+	return v1.giveI() > v2.giveI() ? 1 : (v1.giveI() < v2.giveI() ? -1 : 0);
+};
+
 int main() {
 	setlocale(LC_ALL, "Russian");
 	vector<ImNum<double>> mass;
 	ImNum<double> C;
+	ImNum<double> v1,v2;
 
 	/*заполнение массива*/ {
 		mass.push_back(ImNum<double>(1, 1));//без указания типа видит только первое число
@@ -104,7 +142,25 @@ int main() {
 	//SortInc(mass, CompReal); //работает//классы
 
 	//SortInc(mass, CompIm<double>); //работает//функции
-	SortInc(mass, CompIm); //работает//классы
+	//SortInc(mass, CompIm); //работает//классы
+
+	auto SortInc = [&](auto comp) {
+		size_t len = mass.size();
+		cout << "в массиве " << len << " элементов" << endl;
+		for (int i = 0; i < len; i++) {
+			for (int j = 0; j < len - 1; j++) {
+				if (comp(mass[j], mass[j + 1]) == 1) {
+					C = mass[j];
+					mass[j] = mass[j + 1];
+					mass[j + 1] = C;
+				}
+			}
+		}
+	};
+
+	//SortInc(CompAbs<double>);//работает//лямбда
+	//SortInc(CompReal<double>);//работает//лямбда
+	//SortInc(CompIm<double>);//работает//лямбда
 
 	for (ImNum<double> n : mass)
 		cout << endl << n ;
